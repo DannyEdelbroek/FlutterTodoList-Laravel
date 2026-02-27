75e2c8e684873f6eac2e8469e9405543e334d962
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-02-27 13:41:54 +0100

    Initialzed route(Transaction,  category, register, login en logout)

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+ 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('transactions', TransactionController::class);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
