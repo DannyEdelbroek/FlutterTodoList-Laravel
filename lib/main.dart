8b0bc335d02e40956ac131322abbc529dc852819
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-03-27 13:21:54 +0100

    add home and register so you can excatly register

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import './Screens/Auth/login.dart';
 import './Screens/Auth/register.dart';
 import './Screens/categories/categories_list.dart';
 import 'package:todofrontendapi/providers/category_provider.dart';
+import 'package:todofrontendapi/providers/auth_provider.dart';
+import 'package:todofrontendapi/screens/home.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -14,19 +16,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider<CategoryProvider>(
-              create: (context) => CategoryProvider()),
-        ],
-        child: MaterialApp(
-          title: 'Welcome to Flutter',
-          home: Login(),
-          routes: {
-            '/login': (context) => Login(),
-            '/register': (context) => Register(),
-            '/categories': (context) => CategoriesList(),
-          },
-        ));
-  } 
+    return ChangeNotifierProvider(
+      create: (context) => AuthProvider(),
+      child: Consumer<AuthProvider>(
+        builder: (context, authProvider, child) {
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider<CategoryProvider>(
+                create: (context) => CategoryProvider(),
+              ),
+              ChangeNotifierProvider<AuthProvider>(
+                create: (context) => AuthProvider(),
+              ),
+            ],
+            child: MaterialApp(
+              title: 'Welcome to Flutter',
+              routes: {
+                '/': (context) {
+                  final authProvider = Provider.of<AuthProvider>(context);
+                  return authProvider.isAuthenticated ? Home() : Login();
+                },
+                '/login': (context) => Login(),
+                '/register': (context) => Register(),
+                '/home': (context) => Home(),
+                '/categories': (context) => CategoriesList(),
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
