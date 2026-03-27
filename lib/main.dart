e7c0e2670b216c65fe51e42ccd17d51a59764247
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-03-27 11:30:49 +0100

    add(route): add provider so reload with edit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import './Screens/Auth/login.dart';
 import './Screens/Auth/register.dart';
 import './Screens/categories/categories_list.dart';
+import 'package:todofrontendapi/providers/category_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,14 +14,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Login(),
-      routes: {
-        '/login': (context) => Login(),
-        '/register': (context) => Register(),
-        '/categories': (context) => CategoriesList(),
-      },
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<CategoryProvider>(
+              create: (context) => CategoryProvider()),
+        ],
+        child: MaterialApp(
+          title: 'Welcome to Flutter',
+          home: Login(),
+          routes: {
+            '/login': (context) => Login(),
+            '/register': (context) => Register(),
+            '/categories': (context) => CategoriesList(),
+          },
+        ));
   } 
 }
