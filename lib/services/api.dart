9c900a92918edc65cd932fbb0735fc36794f3a79
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-03-27 13:23:19 +0100

    register work on inputs

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -49,10 +49,8 @@ class ApiService {
 
   Future<void> deleteCategory(id) async {
     String url = '$baseUrl/api/categories/$id';
-    final http.Response response = await http.delete(
-      Uri.parse(url),
-    );
- 
+    final http.Response response = await http.delete(Uri.parse(url));
+
     if (response.statusCode != 204) {
       throw Exception('Failed to delete category');
     }
@@ -65,16 +63,53 @@ class ApiService {
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: jsonEncode(<String, String>{
-        'name': name,
-      }),
+      body: jsonEncode(<String, String>{'name': name}),
     );
- 
+
     if (response.statusCode != 201) {
       throw Exception('Failed to create category');
     }
- 
+
     final Map<String, dynamic> data = json.decode(response.body);
     return Category.fromJson(data['data']);
-}
+  }
+
+  Future<String> register(
+    String name,
+    String email,
+    String password,
+    String password_confirm,
+    String device_name,
+  ) async {
+    String url = '$baseUrl/api/auth/register';
+    final http.Response response = await http.post(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json',
+      },
+      body: jsonEncode(<String, String>{
+        'name': name,
+        'email': email,
+        'password': password,
+        'password_confirmation': password_confirm,
+        'device_name': device_name,
+      }),
+    );
+
+    if (response.statusCode == 422) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final Map<String, dynamic> errors = data['errors'];
+      String message = '';
+      errors.forEach((key, value) {
+        value.forEach((error) {
+          message += '$error\n';
+        });
+      });
+
+      throw Exception(message);
+    }
+
+    return response.body;
+  }
 }
