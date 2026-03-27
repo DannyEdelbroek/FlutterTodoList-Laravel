8adc853d0417b6b26e7687eb44576fa1cba3f54d
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-03-27 11:32:42 +0100

    add(provider) so you can edit and reload the page

---
+++ lib/providers/category_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../services/api.dart';
+import '../models/category.dart';
+
+class CategoryProvider extends ChangeNotifier {
+  List<Category> categories = [];
+  late ApiService apiService;
+ 
+  CategoryProvider() {
+    apiService = ApiService();
+    init();
+  }
+ 
+  Future init() async {
+    try {
+      categories = await apiService.fetchCategories();
+    } catch (e) {
+      print('Failed to load categories: $e');
+    }
+    notifyListeners();
+  }
+
+  Future updateCategory(Category category) async {
+    try {
+      Category updatedCategory = await apiService.saveCategory(category);
+      int index = categories.indexOf(category);
+      categories[index] = updatedCategory;
+      notifyListeners();
+    } catch (e) {
+      print('Failed to update category: $e');
+    }
+  }
+}
