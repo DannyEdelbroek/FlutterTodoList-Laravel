0abf66b651717f99150d8724c7cd201c8a38a3df
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-03-27 14:39:22 +0100

    change layout of homepage

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todofrontendapi/screens/transactions/list.dart';
+import 'package:todofrontendapi/screens/categories/categories_list.dart';
+import 'package:todofrontendapi/providers/auth_provider.dart';
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
  
-class Home extends StatelessWidget {
+class _HomeState extends State<Home> {
+  int selectedIndex = 0;
+  List<Widget> widgetOptions = [Transactions(), CategoriesList()];
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Logged in!'),
-        ),
-        body: Center(
-          child: Text('Welcome',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+        body: widgetOptions.elementAt(selectedIndex),
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 4,
+          child: OverflowBox(
+            maxHeight: double.infinity,
+            child: BottomNavigationBar(
+              backgroundColor: Theme.of(context).primaryColor.withAlpha(0),
+              elevation: 0,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.account_balance_wallet),
+                    label: 'Transactions'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.category), label: 'Categories'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.logout), label: 'Log out')
+              ],
+              currentIndex: selectedIndex,
+              onTap: onItemTapped,
+            ),
+          ),
         ),
       ),
     );
   }
+ 
+  Future<void> onItemTapped(int index) async {
+    if (index == 2) {
+      final AuthProvider provider =
+          Provider.of<AuthProvider>(context, listen: false);
+      await provider.logout();
+    } else {
+      setState(() {
+        selectedIndex = index;
+      });
+    }
+  }
 }
+
